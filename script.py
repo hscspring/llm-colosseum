86e5c905ebcd039eb7b4697ee7a50d39904bf07f
hscspring <haoshaochun@gmail.com>
2024-04-08 17:39:54 +0800

    init

--- script.py
+++ script.py
@@ -5,21 +5,21 @@ from eval.game import Game, Player1, Player2, generate_random_model
 from agent.config import MODELS
 
 
-all_models = MODELS["GLM"] | MODELS["QWEN"]
-
-p1 = p2 = "init"
-if len(sys.argv) < 2:
-    while p1 == p2:
-        p1 = generate_random_model(glm=True, qwen=True)
-        p2 = generate_random_model(glm=True, qwen=True)
-else:
-    p1 = sys.argv[1]
-    p2 = sys.argv[2] 
-
-assert p1 in all_models and p2 in all_models
-print(f"{p1} VS {p2}")
 
 def main():
+    all_models = MODELS["GLM"] | MODELS["QWEN"]
+    p1 = p2 = "init"
+    if len(sys.argv) < 2:
+        while p1 == p2:
+            p1 = generate_random_model(glm=True, qwen=True)
+            p2 = generate_random_model(glm=True, qwen=True)
+    else:
+        p1 = sys.argv[1]
+        p2 = sys.argv[2] 
+
+    assert p1 in all_models and p2 in all_models
+    print(f"{p1} VS {p2}")
+
     game = Game(
         render=True,
         player_1=Player1(
